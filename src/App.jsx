ae80a9cd7828bbf994ff03342b1894d970646f83
Asad Ashfaq <asadashfaq101@gmail.com>
2025-11-20 02:37:31 +0500

    Fix alignment, add React Router, update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import LoginSignup from './pages/LoginSignup.jsx';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  return <LoginSignup />;
+import LoginSignupPage from "./pages/LoginSignup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginSignupPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
 }
-
-export default App;
-
