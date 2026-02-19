970c93dffdf8dc6899c0a5ddb72dcdc4331dbce6
Asad Ashfaq <asadashfaq101@gmail.com>
2026-02-19 12:45:11 +0500

    Fix axios baseURL to VITE_API_URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/token";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 instance.interceptors.request.use((config) => {
